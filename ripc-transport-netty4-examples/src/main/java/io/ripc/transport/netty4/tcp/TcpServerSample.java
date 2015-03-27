70956d498cda47ef7ee1654947788cd1efed0367
Nitesh Kant <nkant@netflix.com>
2015-03-27 16:16:04 -0700

    Adding a short-circuit interception sample.

--- ripc-transport-netty4-examples/src/main/java/io/ripc/transport/netty4/tcp/TcpServerSample.java
+++ ripc-transport-netty4-examples/src/main/java/io/ripc/transport/netty4/tcp/TcpServerSample.java
@@ -8,6 +8,8 @@ import io.ripc.protocol.tcp.TcpInterceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 import static java.nio.charset.Charset.defaultCharset;
 import static rx.RxReactiveStreams.*;
 
@@ -22,9 +24,23 @@ public class TcpServerSample {
         };
     }
 
+    public static TcpInterceptor<ByteBuf, ByteBuf, ByteBuf, ByteBuf> shortCircuitAltConnection() {
+        return handler -> {
+            final AtomicLong connCounter = new AtomicLong();
+            return input -> {
+                if (connCounter.incrementAndGet() % 2 == 0) {
+                    logger.error("Short-circuiting further processing.");
+                    return input.write(Publishers.just(Unpooled.buffer().writeBytes("Go Away!!! \n".getBytes())));
+                }
+                return handler.handle(input);
+            };
+        };
+    }
+
     public static void main(String[] args) {
         Netty4TcpServer.<ByteBuf, ByteBuf>create(0)
                        .intercept(log())
+                       .intercept(shortCircuitAltConnection())
                        .start(connection ->
                                       toPublisher(toObservable(connection)
                                                           .flatMap(byteBuf -> {
