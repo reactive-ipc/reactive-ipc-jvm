32b7c2f8c5266efe9926d74e407d700f5521a33b
Rossen Stoyanchev <rstoyanchev@pivotal.io>
2015-06-05 15:04:14 -0400

    Move SubscriberListener back inside TcpConnectionImpl

--- ripc-transport-netty4/src/main/java/io/ripc/transport/netty4/tcp/TcpConnectionImpl.java
+++ ripc-transport-netty4/src/main/java/io/ripc/transport/netty4/tcp/TcpConnectionImpl.java
@@ -1,8 +1,9 @@
 package io.ripc.transport.netty4.tcp;
 
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
 import io.ripc.protocol.tcp.TcpConnection;
-import io.ripc.transport.netty4.SubscriberListener;
 import io.ripc.transport.netty4.tcp.ChannelToConnectionBridge.ConnectionInputSubscriberEvent;
 import org.reactivestreams.Publisher;
 import org.reactivestreams.Subscriber;
@@ -20,16 +21,33 @@ public class TcpConnectionImpl<R, W> implements TcpConnection<R, W> {
         return new Publisher<Void>() {
             @Override
             public void subscribe(Subscriber<? super Void> s) {
-                nettyChannel.write(data)
-                        .addListener(new SubscriberListener(s));
+                nettyChannel.write(data).addListener(new FutureToSubscriberBridge(s));
             }
         };
     }
 
     @Override
     public void subscribe(Subscriber<? super R> s) {
-        nettyChannel.pipeline()
-                .fireUserEventTriggered(new ConnectionInputSubscriberEvent<>(s));
+        nettyChannel.pipeline().fireUserEventTriggered(new ConnectionInputSubscriberEvent<>(s));
+    }
+
+
+    private static class FutureToSubscriberBridge implements ChannelFutureListener {
+
+        private final Subscriber<? super Void> subscriber;
+
+        public FutureToSubscriberBridge(Subscriber<? super Void> subscriber) {
+            this.subscriber = subscriber;
+        }
+
+        @Override
+        public void operationComplete(ChannelFuture future) throws Exception {
+            if (future.isSuccess()) {
+                subscriber.onComplete();
+            } else {
+                subscriber.onError(future.cause());
+            }
+        }
     }
 
 }
