d616bffadf1284118568be83745177272f82241a
Stephane Maldini <smaldini@gopivotal.com>
2015-07-03 14:12:19 +0100

    fix specification, down to 13 tests failing

--- ripc-core/src/test/java/io/ripc/internal/PublishersTests.java
+++ ripc-core/src/test/java/io/ripc/internal/PublishersTests.java
@@ -3,6 +3,7 @@ package io.ripc.internal;
 import org.reactivestreams.Publisher;
 import org.reactivestreams.tck.PublisherVerification;
 import org.reactivestreams.tck.TestEnvironment;
+import org.testng.SkipException;
 
 /**
  * @author Stephane Maldini
@@ -14,15 +15,18 @@ public class PublishersTests extends PublisherVerification<Integer> {
 
 	@Override
 	public Publisher<Integer> createPublisher(long elements) {
-		if(elements > 1000) throw new UnsupportedOperationException("Unsupported Publisher");
+
 		int size = (int)elements;
 		Integer[] data = new Integer[size];
+		for(int i = 0; i < data.length; i++){
+			data[i] = i;
+		}
 		return Publishers.just(data);
 	}
 
 	@Override
 	public void required_spec317_mustSupportAPendingElementCountUpToLongMaxValue() throws Throwable {
-		//IGNORE
+		throw new SkipException("Not Yet Implemented");
 	}
 
 	@Override
