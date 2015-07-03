96d1e74d239361159052557f763ed030a0dd83ab
Stephane Maldini <smaldini@gopivotal.com>
2015-07-03 14:04:10 +0100

    fixed 2 tck tests relative to error publisher

--- ripc-core/src/main/java/io/ripc/internal/Publishers.java
+++ ripc-core/src/main/java/io/ripc/internal/Publishers.java
@@ -30,20 +30,24 @@ public class Publishers {
         };
     }
 
+    private static final Subscription ERROR_SUB = new Subscription() {
+        @Override
+        public void request(long n) {
+            //ignore
+        }
+
+        @Override
+        public void cancel() {
+            //ignore
+        }
+    };
+
     public static <T> Publisher<T> error(final Throwable t) {
         return new Publisher<T>() {
             @Override
             public void subscribe(final Subscriber<? super T> s) {
-                s.onSubscribe(new Subscription() {
-                    @Override
-                    public void request(long n) {
-                        s.onError(t);
-                    }
-
-                    @Override
-                    public void cancel() {
-                    }
-                });
+                s.onSubscribe(ERROR_SUB);
+                s.onError(t);
             }
         };
     }
